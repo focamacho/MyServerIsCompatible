63d18a8c5fb442b0babf6063d603ff20d58e91d4
Focamacho <renancolombomail@gmail.com>
2021-10-08 04:40:11 -0300

    Update Forge to 37.0.81+. Fixes #2

--- src/main/java/com/focamacho/myserveriscompatible/mixin/MixinClientHooks.java
+++ src/main/java/com/focamacho/myserveriscompatible/mixin/MixinClientHooks.java
@@ -3,11 +3,11 @@ package com.focamacho.myserveriscompatible.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.client.multiplayer.ServerData;
-import net.minecraftforge.fmlclient.ClientHooks;
+import net.minecraftforge.client.ForgeHooksClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(value = ClientHooks.class, remap = false)
+@Mixin(value = ForgeHooksClient.class, remap = false)
 public class MixinClientHooks {
 
     /**
